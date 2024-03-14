e3608d6ab3f9664bcc9d806bd7be80dbc6bc804a
upccore <upccore@list.ru>
2024-03-14 19:32:28 +0300

    Selection sort added

--- algorithms.py
+++ algorithms.py
@@ -1,5 +1,5 @@
-#Бинарный поиск (log n)
-def binary_search(list, item):
+#Бинарный поиск (O(log n))
+def binarySearch(list, item):
     low = 0
     high = len(list) - 1
 
@@ -16,5 +16,25 @@ def binary_search(list, item):
 
 my_list = [1, 3, 5, 7, 9]
 
-print(binary_search(my_list, 7))
-print(binary_search(my_list, 2))
+print(binarySearch(my_list, 7))
+print(binarySearch(my_list, 2))
+
+
+#Сортировка выбором (O(n2))
+def findSmallest(array):
+    smallest = array[0]
+    smallest_index = 0
+    for i in range(1, len(array)):
+        if array[i] < smallest:
+            smallest = array[i]
+            smallest_index = i
+    return smallest_index
+
+def selectionSort(array):
+    new_array = []
+    for i in range(len(array)):
+        smallest = findSmallest(array)
+        new_array.append(array.pop(smallest))
+    return new_array
+
+print(selectionSort([5, 3, 6, 2, 10]))
